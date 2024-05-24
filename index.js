d218752e745becf7f1faf5f75c62748ebac0fe5e
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-05-24 08:31:18 -0700

    rename port and add script to start server

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.get("/", (_req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port localhost:${PORT}.`);
+    console.log(`Server is running on port http://localhost:${PORT}.`);
 });
