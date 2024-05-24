993e76dcc513670a4ac255349225388141c5cf01
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-05-24 08:18:11 -0700

    server initialization

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/", (_req, res) => {
+    res.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port localhost:${PORT}.`);
+});
